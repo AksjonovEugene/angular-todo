dfcb09830283dfbdd4040ffe50b9fe92aead9aee
EvhenAksionov <aksionov.evhen@gmail.com>
2025-08-16 00:36:45 +0300

    додав компоненти todo-list i archive довав route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {TodoPage} from "./pages/todo-page/todo-page";
+import {Archive} from "./pages/archive/archive";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: TodoPage},
+    {path: 'archive', component: Archive}
+];
