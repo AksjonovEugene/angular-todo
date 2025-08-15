2946bd5945ad8e47b3b7f00c9d38d54d3c2710dc
EvhenAksionov <aksionov.evhen@gmail.com>
2025-08-15 16:51:14 +0300

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,50 @@
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+export interface TodoItem {
+  id: number;
+  task: string;
+  completed: boolean;
+  isEdting?: boolean;
+}
+
+@Component({
+  selector: 'app-root',
+  imports: [FormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+
+  todoList: TodoItem [] = []
+  newTask: string = '';
+
+  addTask(): void {
+    if (this.newTask.trim() !== '') {
+
+      const newTodoItem: TodoItem = {
+        id: Date.now(),
+        task: this.newTask,
+        completed: false
+      }
+      this.todoList.push(newTodoItem)
+      this.newTask = ''
+    }
+  }
+
+  toggleCompleted(index: number): void {
+    this.todoList[index].completed = !this.todoList[index].completed
+  }
+
+  deleteTask(id: number): void {
+    this.todoList = this.todoList.filter(item => item.id !== id)
+    console.log(this.todoList);
+  }
+
+  toggleEdit(editTask: TodoItem) {
+    if (editTask.isEdting) {
+      editTask.task = editTask.task.trim()
+    }
+    editTask.isEdting = !editTask.isEdting;
+  }
+}
